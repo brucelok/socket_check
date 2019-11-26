0b759cf115c372c28a30ceac3d17bef45763c6cb
Bruce <lok.bruce@gmail.com>
2019-11-26 07:55:22 +0000

    tweak socket connection with multi-processing

--- check_batch_ip_port.py
+++ check_batch_ip_port.py
@@ -1,41 +1,58 @@
 import sys, socket
+from multiprocessing import Process
 '''
 a simple tool that read a list of IP addresses/ports from a plaintext file
-and verify the remote IP/Port connection.
+and verify the remote IP/Port connection in parallel.
 example file format (comma is required as a delimiter btw IP and Port):
 10.0.0.1,8443
 10.0.0.2,22
 syntax:
 $ python check_batch_ip_port.py <text filename>
-author: lok.bruce@gmail.com
+author: BruceMC_Lok@cncbinternational.com
 '''
-if len(sys.argv) < 2:
-    sys.stderr.write("Usage: $ python %s <text_filename>" % sys.argv[0])
-    sys.exit(1)
+def socket_connect(ip, port):
+    '''
+    desc: use socket() to connect target IP and Port
+    args: IP, port
+    return: string
+    '''
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(3)
+    try:
+        s.connect((ip, int(port)))
+        print('connected to ' + ip + ':' + port)
+    except socket.error as msg:
+        print('failed to '+ ip + ':' + port + ' ' + str(msg) +'\n')
+        pass
+    s.close()
 
-textfile = sys.argv[1]  #take filename in same directory
+if __name__ == "__main__":
+    # check if argument <filename> is entered
+    if len(sys.argv) < 2:
+        sys.stderr.write("Usage: $ python %s <text_filename>" % sys.argv[0])
+        sys.exit(1)
 
-try:
-    f = open(textfile, 'r')
-except OSError as e:
-    print('failed to open '+ textfile + ' ' + str(e))
-    sys.exit(1)
+    textfile = sys.argv[1]  #take filename in same directory
+    # check if filename is existed
+    try:
+        f = open(textfile, 'r')
+    except OSError as e:
+        print('failed to open '+ textfile + ' ' + str(e))
+        sys.exit(1)
+    
+    procs = []
+    with f:
+        for line in f:
+            awk = line.split(",")
+            ip = awk[0]
+            port = awk[1]
+            
+            # instantiate process with arguments(ip, port)
+            proc = Process(target=socket_connect, args=(ip, port,))
+            procs.append(proc)
+            proc.start()    # start multiple processes to connect socket
 
-with f:
-    for line in f:
-        awk = line.split(",")
-        ip = awk[0]
-        port = awk[1]
-        
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(3)
-        try:
-            s.connect((ip, int(port)))
-            print('connected to ' + ip + ':' + port)
+        for proc in procs:
+            proc.join()    # recycle all processes
 
-        except socket.error as msg:
-            print('failed to '+ ip + ':' + port + ' ' + str(msg) +'\n')
-            pass
-        s.close()
-        
-print('### END OF TEST ###')
+    print('### END OF TEST ###')
